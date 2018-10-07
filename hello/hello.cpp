b94582a86dc7d0a342edccbd397cd50e00380103
snguyen26 <sydneynguyen26@gmail.com>
2018-10-07 14:19:40 -0700

    Hello User

---
+++ hello/hello.cpp
@@ -0,0 +1,21 @@
+//
+//  hello.cpp
+//
+
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/print.hpp>
+
+using namespace eosio;
+
+class hello : public contract {
+public:
+    using contract::contract;
+
+    [[eosio::action]]
+    void hi (account_name user) {
+        require_auth( user ); // ensures the authorizing user must be the same as
+                                // the user we are saying "hi" to
+        print ("Hello, ", name{user} );
+    }
+};
+EOSIO_ABI( hello, (hi))
